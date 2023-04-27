36587309ffd29dea48aa79154dad6effbcf764b3
pabloramoss <pablo-362@hotmail.com>
2023-04-27 23:00:09 +0200

    delete prop winningNumbers square component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -4,7 +4,7 @@ interface Props {
   onClick: () => void;
   winningNumbers: number[];
 }
-const Square: React.FC<Props> = ({number, selected, onClick, winningNumbers})  => {
+const Square: React.FC<Props> = ({number, selected, onClick})  => {
 
   return (
     <div style={{width: "50px", height: "50px", border: `1px solid ${selected ? "red" : "grey"}`, display: 'flex', alignItems: 'center', justifyContent: 'center'}} onClick={onClick}>
